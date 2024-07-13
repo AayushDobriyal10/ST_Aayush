9f6e0cb89a1c34b8ffc0230f32d1000139a04a22
AayushDobriyal10 <aayushdobriyal10@gmail.com>
2024-07-13 13:37:16 +0530

    Questions

---
+++ Week_0/Day2/PatternAssignment.java
@@ -0,0 +1,98 @@
+package Day2;
+
+public class PatternAssignment {
+    public static void main(String[] args) {
+
+        int n = 5;
+        //STAR PATTERNS
+
+        //Question 1 
+        System.out.println("Question 1");
+        for(int i = 1 ; i<=n ; i++){
+            for(int j =1 ; j <= n ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Question 2 
+        System.out.println("Question 2");
+        for(int i = 1 ; i <= n ; i++){
+            for(int j =1 ; j <= i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        //Question 3
+        System.out.println("Question 3");
+        for(int i = n ; i >= 1 ; i--){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Question 4
+        System.out.println("Question 4");
+        for(int i = 1 ;  i <= n ; i++){
+            for(int k = 1 ; k <=(n-i); k++){
+                System.out.print("  ");
+            }
+            for(int j = 1 ; j<=i ; j++){
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        //Question 5
+        System.out.println("Question 5");
+        for(int i = n ; i >=1 ; i --){
+            for(int k = 1 ; k <= (n-i); k++){
+                System.out.print("  ");
+            }
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Question 6
+        System.out.println("Question 6");
+        for(int i = n ; i >=1 ; i --){
+            for(int k = 1 ; k <= (n-i); k++){
+                System.out.print("    ");
+            }
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Question 7
+        System.out.println("Question 7");
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                if( i == 1 || i == n || j == 1 || j == n ){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

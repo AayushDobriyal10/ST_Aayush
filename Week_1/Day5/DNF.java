3f2e5df57d23d7248e2ff8da20a1ae19c8cd92b3
AayushDobriyal10 <aayushdobriyal10@gmail.com>
2024-07-22 14:06:12 +0530

    commit

---
+++ Week_1/Day5/DNF.java
@@ -0,0 +1,26 @@
+package Week_1.Day5;
+
+public class DNF {
+    public static void swap(int[] arr , int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums, low, mid);
+                low++; mid++;
+            }
+            else if ( nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums, high, mid);
+                high--;
+            }
+        }
+    }
+}

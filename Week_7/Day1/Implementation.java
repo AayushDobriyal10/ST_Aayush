2785bfb5d1d607feee5a49cddaa1f5e10f8b743b
AayushDobriyal10 <aayushdobriyal10@gmail.com>
2024-10-21 20:13:26 +0530

    Week7 Day1

---
+++ Week_7/Day1/Implementation.java
@@ -0,0 +1,19 @@
+package Week_7.Day1;
+
+public class Implementation {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        // bt.Display();
+        // System.out.println();
+        // System.out.println();
+
+        // System.out.println(bt.findMax(bt.root));
+        // System.out.println(bt.findElement(bt.root,4));
+        // System.out.println(bt.treeHeight(bt.root));
+        bt.preOrder(bt.root);
+        System.out.println();
+        bt.inOrder(bt.root);
+        System.out.println();
+        bt.postOrder(bt.root);
+    }
+}
